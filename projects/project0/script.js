314830ec48fc5c5dcffb890aacc1d215ad012c81
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2024-09-02 00:20:09 -0300

    updating itemCountSpan

--- projects/project0/script.js
+++ projects/project0/script.js
@@ -9,22 +9,30 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
-let counter = 0
+let itemCounter = 0
 
 function newTodo() {
-  counter++
+  itemCounter++
 
   todoName = document.getElementById('todo-name')
 
   let li = document.createElement('li')
   li.className = classNames.TODO_ITEM
 
-  let todoText = todoName.value ? todoName.value : counter + 'º TODO'
+  let todoText = todoName.value ? todoName.value : itemCounter + 'º TODO'
   li.innerHTML = `<span class="${classNames.TODO_TEXT}">${todoText}</span>`
-  li.innerHTML += `<input type="checkbox" class="${classNames.TODO_CHECKBOX}" id="checkbox-${counter}">`
+  li.innerHTML += `<input type="checkbox" class="${classNames.TODO_CHECKBOX}" id="checkbox-${itemCounter}">`
 
   list.appendChild(li)
 
   todoName.value = ''
   todoName.focus()
+
+  updateCountSpans()
+}
+
+function updateCountSpans() {
+  itemCountSpan.innerHTML = itemCounter
+
+  // TODO: update uncheckedCountSpan
 }
