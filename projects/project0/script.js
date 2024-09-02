05e32ec3350f371b608303a9189a3815660d6437
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2024-09-02 00:05:10 -0300

    making newTodo() function

--- projects/project0/script.js
+++ projects/project0/script.js
@@ -9,6 +9,22 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+let counter = 0
+
 function newTodo() {
-  alert('New TODO button clicked!')
+  counter++
+
+  todoName = document.getElementById('todo-name')
+
+  let li = document.createElement('li')
+  li.className = classNames.TODO_ITEM
+
+  let todoText = todoName.value ? todoName.value : counter + 'º TODO'
+  li.innerHTML = `<span class="${classNames.TODO_TEXT}">${todoText}</span>`
+  li.innerHTML += `<input type="checkbox" class="${classNames.TODO_CHECKBOX}" id="checkbox-${counter}">`
+
+  list.appendChild(li)
+
+  todoName.value = ''
+  todoName.focus()
 }
