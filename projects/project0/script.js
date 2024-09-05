cc102ba1f492f8a92bf976e6921c8aaa2f4c0840
OLuizFernando <luizfernandodematoscarvalho@gmail.com>
2024-09-05 00:12:20 -0300

    Update script.js

--- projects/project0/script.js
+++ projects/project0/script.js
@@ -14,25 +14,40 @@ let itemCounter = 0
 function newTodo() {
   itemCounter++
 
-  todoName = document.getElementById('todo-name')
+  const todoName = document.getElementById('todo-name')
 
   let li = document.createElement('li')
   li.className = classNames.TODO_ITEM
 
-  let todoText = todoName.value ? todoName.value : itemCounter + 'º TODO'
+  const todoText = todoName.value ? todoName.value : `${itemCounter}º TODO`
   li.innerHTML = `<span class="${classNames.TODO_TEXT}">${todoText}</span>`
-  li.innerHTML += `<input type="checkbox" class="${classNames.TODO_CHECKBOX}" id="checkbox-${itemCounter}">`
+  li.innerHTML += `<input type="checkbox" class="${classNames.TODO_CHECKBOX}" value="${itemCounter}">`
 
   list.appendChild(li)
 
+  // Clear the input field and focus for the next item
   todoName.value = ''
   todoName.focus()
 
+  // Update the counters and add the listener to the new checkbox
   updateCountSpans()
+  addCheckboxListener(li.querySelector(`.${classNames.TODO_CHECKBOX}`))
 }
 
+// Update the counters
 function updateCountSpans() {
   itemCountSpan.innerHTML = itemCounter
+  uncheckedCountSpan.innerHTML = itemCounter - countChecked()
+}
 
-  // TODO: update uncheckedCountSpan
+// Count how many checkboxes are checked
+function countChecked() {
+  const checkboxes = document.getElementsByClassName(classNames.TODO_CHECKBOX)
+  const checkedCount = Array.from(checkboxes).filter(checkbox => checkbox.checked).length
+  return checkedCount
+}
+
+// Add event listener to the checkbox to update counts on check/uncheck
+function addCheckboxListener(checkbox) {
+  checkbox.addEventListener('change', updateCountSpans)
 }
